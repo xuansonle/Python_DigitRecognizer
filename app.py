6772f21f7e10313cd2acc068769a02dab7119efd
Xuan Son Le <lexuanson1992@gmail.com>
2020-05-26 23:15:38 +0200

    base.html added

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from flask import Flask, render_template, url_for, request
 import pickle
 import base64
 import numpy as np
-from helpfunctions import predictNumber, dataPrep
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from cv2 import cv2
+from PIL import Image
+
+# Load pre-trained model
+model = load_model("model.h5")
 
 #Initialize the useless part of the base64 encoded image.
 init_Base64 = 21
@@ -20,21 +26,32 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        final_pred = None
+        prediction = None
+        predictions = None
         #Preprocess the image : set the image to 28x28 shape
         #Access the image
         draw = request.form['url']
+
         #Removing the useless part of the url.
         draw = draw[init_Base64:]
         #Decoding
         draw_decoded = base64.b64decode(draw)
         image = np.asarray(bytearray(draw_decoded), dtype="uint8")
-    
-        image = dataPrep(image)
-        final_pred = predictNumber(image)
-        #print(final_pred)
+        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+        
+        # Resizing and reshaping to keep the ratio.
+        image = cv2.resize(image, (28,28), interpolation = cv2.INTER_AREA)
+        image = np.asarray(image, dtype="uint8")
+        
+        image = image.reshape(-1, 28, 28, 1).astype('float32')
+        image /= 255.0
+        
+        # Get the prediction
+        predictions = list(map(lambda x: round(x,2), list(model.predict(image))[0]))
+        prediction = np.argmax(predictions)
+        predictions = dict(zip(list(range(0,10)),predictions))
 
-    return render_template('results.html', prediction=final_pred)
+    return render_template('results.html', prediction=prediction, predictions=predictions)
 
 
 if __name__ == '__main__':
